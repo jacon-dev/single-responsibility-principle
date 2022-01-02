8a2e8814f4c3b59c5fb75add87a7817723e2fdc3
James Conlin <jacon.dev@outlook.com>
2022-01-02 14:16:55 +0000

    Initial Commit

---
+++ Bug.cs
@@ -0,0 +1,28 @@
+﻿namespace SingleResponsibility
+{
+    public class Bug
+    {
+        public Bug(string nameOfChange)
+        {
+            Name = nameOfChange;
+        }
+        
+        public string Name { get; }
+        
+        public int DevelopmentHours { get; set; }
+        
+        public int TestHours { get; set; }
+
+        public bool IsPlanned { get; set; }
+        
+        public bool IsDeveloped { get; set; }
+        
+        public bool IsTested { get; set; }
+        
+        public bool IsApproved { get; set; }
+        
+        public string Approver { get; set; }
+        
+        public bool IsReleased { get; set; }
+    }
+}
---
+++ BugLifeCycle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SingleResponsibility
+{
+    public class BugLifeCycle
+    {
+        private Bug _bug;
+
+        public void CustomerReportsTheBug()
+        {
+            _bug = new Bug("Single Responsibility Principle Bug");
+        }
+
+        public void ProductPlansTheWork()
+        {
+            if (_bug != null && string.IsNullOrEmpty(_bug.Name) == false)
+            {
+                _bug.TestHours = 4;
+                _bug.DevelopmentHours = 6;
+                _bug.IsPlanned = true;
+            }
+        }
+
+        public void DeveloperFixesTheBug()
+        {
+            if (_bug != null && _bug.IsPlanned)
+            {
+                Console.WriteLine("Doing some coding...");
+                _bug.IsDeveloped = true;
+            }
+        }
+
+        public void TesterDoesTheTesting()
+        {
+            if (_bug != null && _bug.IsDeveloped)
+            {
+                Console.WriteLine("Doing some testing...");
+                _bug.IsTested = true;
+            }
+        }
+
+        public void ApproveTheBug()
+        {
+            if (_bug != null && _bug.IsDeveloped && _bug.IsTested)
+            {
+                _bug.Approver = "JC#";
+                _bug.IsApproved = true;
+            }
+        }
+
+        public void ReleaseTheFix()
+        {
+            if (_bug != null && _bug.IsApproved)
+            {
+                Console.WriteLine($"Releasing the fix for bug {_bug.Name} approved by {_bug.Approver}");
+                _bug.IsReleased = true;
+            }
+        }
+    }
+}
